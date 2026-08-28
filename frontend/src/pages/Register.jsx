3187eaaba15cbfa07a27e23cbe8b59f37354ecc8
pratikShewane369 <mohanshewane@gmail.com>
2026-08-28 19:39:00 +0530

    Changes made in Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -9,7 +9,6 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
